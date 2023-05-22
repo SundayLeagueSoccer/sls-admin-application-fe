8f4cf5e568917e25c7708b0c8d7ef7e989b8db7e
Henry <aguhenrychuks@gmail.com>
2023-05-22 20:33:10 +0100

    feat: landing page updated

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,11 +1,38 @@
+// stylesheet
 import styles from "./LandingPage.module.scss";
+
+// Framer motion
 import AnimatedFadeInPage from "../../utils/AnimatedFadeInPage";
+
+// React link
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <>
       <AnimatedFadeInPage>
-        <main className={styles.LandingPage}>LandingPage</main>
+        <main className={styles.LandingPage}>
+          <div className={styles.LandingPageHeader}>
+            <p>What do you want to update?</p>
+          </div>
+          <div className={styles.LandingPageButtons}>
+            <Link to="/">
+              <button>Goals</button>
+            </Link>
+            <Link to="/">
+              <button>Assists</button>
+            </Link>
+            <Link to="/">
+              <button>Clean Sheets</button>
+            </Link>
+            <Link to="/">
+              <button>Red Cards</button>
+            </Link>
+            <Link to="/">
+              <button>Yellow Cards</button>
+            </Link>
+          </div>
+        </main>
       </AnimatedFadeInPage>
     </>
   );
